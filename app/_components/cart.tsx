c7d905170ff58d7285f44aae18f1d42e5a395bb2
Jackson-Timoteo <60791082+Jackson-Timoteo@users.noreply.github.com>
2024-05-16 11:18:01 -0300

    feat: ensure order can be created

--- app/_components/cart.tsx
+++ app/_components/cart.tsx
@@ -9,9 +9,20 @@ import { createOrder } from "../_actions/order";
 import { OrderStatus } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import { Loader2 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "./ui/alert-dialog";
 
 const Cart = () => {
   const [isSubmitLoading, setIsSubmitLoading] = useState(false);
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false);
 
   const { data } = useSession();
 
@@ -49,77 +60,104 @@ const Cart = () => {
   };
 
   return (
-    <div className="flex h-full flex-col py-5">
-      {/* TOTAL */}
+    <>
+      <div className="flex h-full flex-col py-5">
+        {/* TOTAL */}
 
-      {products.length > 0 ? (
-        <>
-          <div className="flex-auto space-y-4">
-            {products.map((product) => (
-              <CarItem key={product.id} cartProduct={product} />
-            ))}
-          </div>
+        {products.length > 0 ? (
+          <>
+            <div className="flex-auto space-y-4">
+              {products.map((product) => (
+                <CarItem key={product.id} cartProduct={product} />
+              ))}
+            </div>
 
-          <div className="mt-6">
-            <Card>
-              <CardContent className="space-y-2 p-5">
-                <div className="flex items-center justify-between text-sm">
-                  <span className="text-muted-fofregund">Subtotal</span>
-                  <span>{formatCurrency(subtotalPrice)}</span>
-                </div>
+            <div className="mt-6">
+              <Card>
+                <CardContent className="space-y-2 p-5">
+                  <div className="flex items-center justify-between text-sm">
+                    <span className="text-muted-fofregund">Subtotal</span>
+                    <span>{formatCurrency(subtotalPrice)}</span>
+                  </div>
 
-                <Separator />
+                  <Separator />
 
-                <div className="flex items-center justify-between text-sm">
-                  <span className=" text-muted-foreground"> Descontos</span>
-                  <span> - {formatCurrency(totalDiscounts)}</span>
-                </div>
+                  <div className="flex items-center justify-between text-sm">
+                    <span className=" text-muted-foreground"> Descontos</span>
+                    <span> - {formatCurrency(totalDiscounts)}</span>
+                  </div>
 
-                <Separator />
+                  <Separator />
 
-                <div className="flex items-center justify-between text-sm">
-                  <span className=" text-muted-foreground">Entrega</span>
+                  <div className="flex items-center justify-between text-sm">
+                    <span className=" text-muted-foreground">Entrega</span>
 
-                  {Number(products?.[0].restaurant.deliveryFee) === 0 ? (
-                    <span className="font-semibold uppercase text-primary">
-                      Grátis
-                    </span>
-                  ) : (
-                    formatCurrency(Number(products?.[0].restaurant.deliveryFee))
-                  )}
-                </div>
+                    {Number(products?.[0].restaurant.deliveryFee) === 0 ? (
+                      <span className="font-semibold uppercase text-primary">
+                        Grátis
+                      </span>
+                    ) : (
+                      formatCurrency(
+                        Number(products?.[0].restaurant.deliveryFee),
+                      )
+                    )}
+                  </div>
 
-                <Separator />
+                  <Separator />
 
-                <div className="flex items-center justify-between text-xs font-semibold">
-                  <span>Total:</span>
-                  <span>{formatCurrency(totalPrice)}</span>
-                </div>
+                  <div className="flex items-center justify-between text-xs font-semibold">
+                    <span>Total:</span>
+                    <span>{formatCurrency(totalPrice)}</span>
+                  </div>
 
-                <Separator />
-              </CardContent>
-            </Card>
-          </div>
+                  <Separator />
+                </CardContent>
+              </Card>
+            </div>
 
-          {/* Finalizar pedido */}
-          <Button
-            className="mt-6 w-full"
-            onClick={handleFinishOrderClick}
-            disabled={isSubmitLoading}
-          >
-            {isSubmitLoading && (
-              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Finalizar pedido
-          </Button>
-        </>
-      ) : (
-        <h2 className="text-center font-medium">
-          Seu carrinho está vazio. Para finalizar adicione pelo menos um
-          produto.
-        </h2>
-      )}
-    </div>
+            {/* Finalizar pedido */}
+            <Button
+              className="mt-6 w-full"
+              onClick={() => setIsConfirmDialogOpen(true)}
+              disabled={isSubmitLoading}
+            >
+              Finalizar pedido
+            </Button>
+          </>
+        ) : (
+          <h2 className="text-center font-medium">
+            Seu carrinho está vazio. Para finalizar adicione pelo menos um
+            produto.
+          </h2>
+        )}
+      </div>
+
+      <AlertDialog
+        open={isConfirmDialogOpen}
+        onOpenChange={setIsConfirmDialogOpen}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Deseja Finalizar seu pedido??</AlertDialogTitle>
+            <AlertDialogDescription>
+              Ao finalizar seu pedido, você concorda com os termos e condições
+              da nossa plataforma.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel disabled={isConfirmDialogOpen}>
+              {isSubmitLoading && (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Cancelar
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={handleFinishOrderClick}>
+              Finalizar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
